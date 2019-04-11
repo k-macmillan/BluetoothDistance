351dda4977e11052dbb5c0e4cee1f7034282cefe
Kyle MacMillan <macattackftw@gmail.com>
2019-04-11 00:27:25 -0600

    Added command line argument check to bash script.

--- bluetooth_scan/get_data.sh
+++ bluetooth_scan/get_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+MISSING="Missing x, y, z variables"
 DATA=bt_data
 DEVICES=devices
 CSV=sensor_data.csv
@@ -8,6 +9,11 @@ SOUND=transfer_complete.wav
 SCAN="Running Bluetooth LE scan..."
 RUNNING="\r\033[1A\033[0K"$SCAN
 JOBSDONE="Job's done..."
+
+if [ "$#" -ne 3 ]; then
+    echo $MISSING
+    exit 1
+fi
 
 echo $SCAN
 
